99439517bb32d3a00f1cac23ae539323fec44ec0
Jonathan Souza <jonathanbcsouza@hotmail.com>
2020-09-13 11:30:36 +1200

    Updates

---
+++ precache-manifest.8b2696d5b15ddb59e78279c15f8397dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c29465ab501b730a7056d1656454ad",
+    "url": "/personalwebsite/index.html"
+  },
+  {
+    "revision": "60ff62d072454d3f1324",
+    "url": "/personalwebsite/static/css/main.777627ae.chunk.css"
+  },
+  {
+    "revision": "dd96d145c6037e03269d",
+    "url": "/personalwebsite/static/js/2.5993bd7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personalwebsite/static/js/2.5993bd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ff62d072454d3f1324",
+    "url": "/personalwebsite/static/js/main.6e14f0ee.chunk.js"
+  },
+  {
+    "revision": "74c08130c02602688ae3",
+    "url": "/personalwebsite/static/js/runtime-main.c30763b0.js"
+  }
+]);
