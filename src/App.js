27825c235a661196abbb1e742ac0509b1b5407b0
Jonathan Souza <jonathanbcsouza@hotmail.com>
2020-09-24 23:52:41 +1200

    Use props

--- src/App.js
+++ src/App.js
@@ -7,13 +7,42 @@ import SocialMedia from './components/SocialMedia';
 
 import "./styles.css";
 
+
+const currentTime = new Date().getHours();
+
+const customStyle = {
+    color: ""
+}
+
+let greeting;
+
+if (currentTime < 12) {
+    greeting = "morning"
+    customStyle.color = "orange"
+} else if (currentTime < 18) {
+    greeting = "afternoon"
+    customStyle.color = "red"
+
+} else {
+    greeting = "evening"
+    customStyle.color = "green"
+
+}
+
+
 function App() {
   return (
+    
     <div className="App">
 
       <Header />
       <ProfilePhoto />
-      <Greetings />
+
+      <h3 style={customStyle}>Good {greeting}.</h3>
+
+      <Greetings name="Jonathan" surname="Souza" sentence="this is my first react component" />
+      <Greetings sentence="and this is the same component that I am reusing it with a new text :)" />
+
       <SocialMedia />
       <Footer />
 
--- src/components/Greetings/index.js
+++ src/components/Greetings/index.js
@@ -1,40 +1,13 @@
 import React from 'react';
 import "./styles.css";
 
-const name = "Jonathan"
-const surname = ""
-const currentTime = new Date().getHours();
-
-const customStyle = {
-    color: ""
-}
-
-let greeting;
-
-if (currentTime < 12) {
-    greeting = "morning"
-    customStyle.color = "orange"
-} else if (currentTime < 18) {
-    greeting = "afternoon"
-    customStyle.color = "red"
-
-} else {
-    greeting = "evening"
-    customStyle.color = "green"
-
-}
-
-
-function Greetings() {
+function Greetings(props) {
     return (
         <div className="greetings">
 
             <br></br>
-            <h3 style={customStyle}>Good {greeting}.</h3>
-            <h3>I am {name} {surname} and I am glad to have you browsing here.</h3>
-            <br>
-            </br>
-            <h3>This webpage is underconstruction and it is turning to an React app .</h3>
+
+            <h3>I am {props.name} {props.surname} {props.sentence}</h3>
 
         </div>
     );
