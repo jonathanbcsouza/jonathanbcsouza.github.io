33d10ae4300d2238ebb27194b60b8b189254849f
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-02-07 15:08:45 +1300

    add Hooks

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <>
       <div className="App">
         {/* <Header setFontSize="12" setText="Welcome" /> */}
-        <Greetings setColour="BurlyWood" />
+        <Greetings />
 
         <BackgroundImage />
 
--- src/components/Greetings/index.js
+++ src/components/Greetings/index.js
@@ -5,53 +5,61 @@ const StyledGreetings = styled.p`
   font-family: "opensans-bold";
   font-size: 24px;
   text-align: center;
-  color: ${({ styledColour }) => `${styledColour}`};
 `;
 
-const currentTime = new Date().getHours();
-let greeting;
+let currentTime;
+const currentTimeStr = new Date().toLocaleTimeString();
 
-const customStyle = {
-  color: "",
-};
-
-if (currentTime < 12) {
-  greeting = "morning";
-  customStyle.color = "#da0132";
-} else if (currentTime < 18) {
-  greeting = "afternoon";
-  customStyle.color = "#orange";
-} else {
-  greeting = "evening";
-  customStyle.color = "#da0132";
-}
-
-const time = new Date().toLocaleTimeString();
-
-const Greetings = ({ setColour }) => {
+const Greetings = () => {
   setInterval(updateTime, 1000);
-  const [getTime, setTime] = useState(time);
-
+  const [time, setTime] = useState(currentTimeStr);
   function updateTime() {
     const newTime = new Date().toLocaleTimeString();
     setTime(newTime);
   }
 
+  setInterval(updateColour, 1000);
+  const [textColour, setTextColour] = useState("#d8b97f");
+  function updateColour() {
+
+    currentTime = new Date().getHours();
+    let newColour;
+
+    if (currentTime < 12) {
+      newColour = "d8b97f";
+    } else if (currentTime < 18) {
+      newColour = "red";
+    } else {
+      newColour = "green";
+    }
+    setTextColour(newColour);
+  }
+
   setInterval(updateGreeting, 1000);
-  const [getGreet, setGreet] = useState(greeting);
+  const [greeting, setGreeting] = useState("...");
   function updateGreeting() {
-    const newGreet = greeting;
-    setGreet(newGreet);
+
+    currentTime = new Date().getHours();
+    let newGreeting;
+
+    if (currentTime < 12) {
+      newGreeting = "morning";
+    } else if (currentTime < 18) {
+      newGreeting = "afternoon";
+    } else {
+      newGreeting = "evening";
+    }
+    setGreeting(newGreeting);
   }
 
   return (
     <>
-      <br></br>
-      <StyledGreetings styledColour={setColour}>
-        <p style={customStyle}>
-          Good {getGreet} - {getTime}
+      <br />
+      <StyledGreetings>
+        <p style={{ color: textColour }}>
+          Good {greeting} - {time}
         </p>
-        <br></br>
+        <br />
       </StyledGreetings>
     </>
   );
