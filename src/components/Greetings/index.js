e6e3a163e9a8ba011faff2c895a188f2c48c916e
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-02-08 17:46:58 +1300

    Clean setInterval
    Nest all functions in only one setInterval, modifify the setStatetement function to handleHookStatemente in order to not confuse developer

--- src/components/Greetings/index.js
+++ src/components/Greetings/index.js
@@ -11,35 +11,36 @@ let currentTime;
 const currentTimeStr = new Date().toLocaleTimeString();
 
 const Greetings = () => {
-  setInterval(updateTime, 1000);
+  
+  const [textColour, setTextColour] = useState("#d8b97f");
+  function updateColour() {
+    currentTime = new Date().getHours();
+    let checkColour;
+    const newColour = handleHookStatement(checkColour, "d8b97f", "red", "green");
+    setTextColour(newColour);
+  }
+  
+  const [greeting, setGreeting] = useState("...");
+  function updateGreeting() {
+    currentTime = new Date().getHours();
+    let checkGreeting;
+    const newGreeting = handleHookStatement(checkGreeting,"morning","afternoon","evening");
+    setGreeting(newGreeting);
+  }
+  
   const [time, setTime] = useState(currentTimeStr);
   function updateTime() {
     const newTime = new Date().toLocaleTimeString();
     setTime(newTime);
   }
 
-  setInterval(updateColour, 1000);
-  const [textColour, setTextColour] = useState("#d8b97f");
-  function updateColour() {
-    currentTime = new Date().getHours();
-    
-    let checkColour;
-    const newColour = setStatement(checkColour, "d8b97f", "red", "green");
-    setTextColour(newColour);
-  }
+  setInterval(() => {
+    updateTime();
+    updateColour();
+    updateGreeting();
+  }, 1000);
 
-  setInterval(updateGreeting, 1000);
-  const [greeting, setGreeting] = useState("...");
-  function updateGreeting() {
-    currentTime = new Date().getHours();
-    
-    let checkGreeting;
-    const newGreeting = setStatement(checkGreeting,"morning", "afternoon", "evening");
-    setGreeting(newGreeting);
-  }
-
-
-  function setStatement(focus, condA, condB, condC) {
+  function handleHookStatement(focus, condA, condB, condC) {
     if (currentTime < 12) {
       focus = condA;
     } else if (currentTime < 18) {
