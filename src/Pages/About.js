0dc141661d66489741b85320c5813324341936d9
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-02-27 11:22:17 +1300

    Fix about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -30,15 +30,21 @@ const About = (props) => {
   return (
     <StyledAbout>
       <h1>Creating,learning, working and repeat.</h1>
-      Over the years I had fun by acquiring experience and learning a bunch of
-      tools that aligned with creativity and resilience, I could help companies to raise their
-      value, their culture and to increase their revenue. I am
-      addicted to innovation and very enthusiastic about learning new things, I
-      am always improvement something :) I am passionate about data, quality and web
-      development as I believe that together, they are powerful and meaningful in many ways to
-      change people's lives. In my free time, I like to stay with my family
-      and friends, learn new stuff, compose songs or write new ideas
-      somewhere.
+      <p>
+        Over the years I had fun by acquiring experience and learning a bunch of
+        tools that aligned with creativity and resilience, I could help
+        companies to raise their value, their culture and to increase their
+        revenue. I am addicted to innovation and very enthusiastic about
+        learning new things, I am always improvement something!
+        <br />
+        I am passionate about data, quality and web development as I
+        believe that together, they are powerful and meaningful in many ways to
+        change people's lives. In my free time, I like to stay with my family
+        and friends, learn new stuff, compose songs or write new ideas
+        somewhere.
+        <br />
+        Should you want to chat with me, feel free to drop a message :)
+      </p>
     </StyledAbout>
   );
 };
