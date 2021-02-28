0c8390e32f70fbdf0c0dd1d485d32eba3058171d
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-03-01 00:03:53 +1300

    Fix cut off

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,51 +1,66 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledAbout = styled.div`
+const StyledContainer = styled.div`
   color: white;
-  padding-top: 15vh;
-  margin: auto;
-  margin: 0 16px;
+  padding: 15% 5%;
   font-size: 16px;
   line-height: 26px;
-  ${"" /* word-wrap: break-word;
-  overflow-wrap: break-word; */}
-  ${"" /* overflow: auto; */}
+  display: flex;
+  height: 100%;
+  padding-bottom: 20%;
 
+  @media screen and (max-width: 615px) {
+    display: block;
+    font-size: 18px;
+    line-height: 30px;
+    padding-bottom: 30%;
+  }
+`;
+
+const StyledImage = styled.img.attrs({
+  src: `https://avatars.githubusercontent.com/u/25882133?s=460&v=4`,
+})`
+  margin-right: 40px;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+`;
+
+const StyledDiv = styled.div`
   h1 {
     font-size: 1.5em;
     font-weight: bold;
     margin-bottom: 16px;
     letter-spacing: 0.05em;
   }
-
-  @media screen and (max-width: 615px) {
-    font-size: 18px;
-    line-height: 30px;
-    margin-left: 16px;
-  }
 `;
 
 const About = (props) => {
   return (
-    <StyledAbout>
-      <h1>Creating,learning, working and repeat.</h1>
-      <p>
-        Over the years I had fun by acquiring experience and learning a bunch of
-        tools that aligned with creativity and resilience, I could help
-        companies to raise their value, their culture and to increase their
-        revenue. I am addicted to innovation and very enthusiastic about
-        learning new things, I am always improvement something!
-        <br />
-        I am passionate about data, quality and web development as I
-        believe that together, they are powerful and meaningful in many ways to
-        change people's lives. In my free time, I like to stay with my family
-        and friends, learn new stuff, compose songs or write new ideas
-        somewhere.
-        <br />
-        Should you want to chat with me, feel free to drop a message :)
-      </p>
-    </StyledAbout>
+    <>
+      <StyledContainer>
+        <StyledImage />
+        <StyledDiv>
+          <h1>Learn, create, work and repeat.</h1>
+          <p>
+            Over the years I had fun by acquiring experience and learning a
+            bunch of tools that aligned with creativity and resilience, I could
+            help companies to raise their value, their culture and to increase
+            their revenue. I am addicted to innovation and very enthusiastic
+            about learning new things, I am always improvement something!
+            <br />
+            I am passionate about data, quality and web development as I believe
+            that together, they are powerful and meaningful in many ways to
+            change people's lives. In my free time, I like to stay with my
+            family and friends, learn new stuff, compose songs or write new
+            ideas somewhere.
+            <br />
+            Should you want to chat with me, feel free to drop a message :)
+          </p>
+        </StyledDiv>
+      </StyledContainer>
+    </>
   );
 };
 
