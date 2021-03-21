7568d79811aa53e4d06ba5428ba3e1a9b191682f
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-03-21 13:46:20 +1300

    Move the timer into Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Greetings from "../components/Greetings/index";
 
 const StyledContainer = styled.div`
-    color: var(--text-white);
     position: absolute;
     top: 200px;
     z-index: 0;
     padding-left: 30vw;
-    width: 100%;
     height: 100%;
-
+    
     h1,
     p {
       animation-name: text_entrance;
-      animation-duration: 3s;
-      animation-delay: 0.5s;
+      animation-duration: 1s;
       animation-fill-mode: both;
     }
     p {
@@ -23,9 +21,11 @@ const StyledContainer = styled.div`
     @keyframes text_entrance {
       from {
         top: 0px;
+        opacity:0;
       }
       to {
         top: 20px;
+        opacity:1;
 ]      }
     }
 
@@ -37,13 +37,38 @@ const StyledContainer = styled.div`
   `;
 
 const Home = () => {
+  const [time, setTime] = useState("loading...");
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  // Iterate the (HSL) negatively from 100% to a limit of 50%
+  // It syncs with the daytime
+  const [colourLightness, setTextColour] = useState("...");
+  function updateColour() {
+    const setLightness = 100 - timeToPercent(timeToString) / 2;
+    setTextColour(setLightness);
+  }
+
+  const [hour, min, sec] = time.split(":");
+  const timeToString = hour + min + sec;
+
+  function timeToPercent() {
+    const total = 235959;
+    return Math.round((timeToString * 100) / total);
+  }
+  setInterval(() => {
+    updateTime();
+    updateColour();
+  }, 1000);
+
   return (
-    <StyledContainer>
-      <h1>Jonathan Souza</h1>
-      <br></br>
-      <p>A Web Developer with passion for the web, innovation and art.</p>
-      <br />
-    </StyledContainer>
+    <>
+      <StyledContainer>
+        <h1>Jonathan Souza </h1>
+        <Greetings setHsl={colourLightness}></Greetings>
+      </StyledContainer>
+    </>
   );
 };
 
--- src/components/Greetings/index.js
+++ src/components/Greetings/index.js
@@ -1,34 +1,16 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const StyledGreetings = styled.div`
-  position: relative;
-  top: 0vh;
+const StyledGreetings = styled.p`
   display: inline-block;
   font-size: smaller;
-  float: right;
-  margin-right: 2%;
   z-index: 3;
-
-  @media (max-width: 615px) {
-    float: left;
-    margin-left: 10px;
-  }
-
-  p {
-    color: hsl(35, 100%, ${(props) => props.colourOfHour + "%"});
-  }
+  color: hsl(35, 100%, ${(props) => props.setLightness + "%"});
 `;
 
 const currentTime = new Date().getHours();
 
-const Greetings = () => {
-  const [time, setTime] = useState("loading...");
-  function updateTime() {
-    const newTime = new Date().toLocaleTimeString();
-    setTime(newTime);
-  }
-
+const Greetings = (props) => {
   const [greeting, setGreeting] = useState("loading...");
   function updateGreeting() {
     let displayGreeting;
@@ -52,33 +34,17 @@ const Greetings = () => {
     setGreeting(displayGreeting);
   }
 
-  // Iterate the (HSL) negatively from 100% to a limit of 50%
-  // It syncs with the daytime
-  const [colourLightness, setTextColour] = useState("...");
-  function updateColour() {
-    const setLightness = 100 - timeToPercent(timeToString) / 2;
-    setTextColour(setLightness);
-  }
-
-  const [hour, min, sec] = time.split(":");
-  const timeToString = hour + min + sec;
-
-  function timeToPercent() {
-    const total = 235959;
-    return Math.round((timeToString * 100) / total);
-  }
-
+  console.log(currentTime)
   setInterval(() => {
-    updateTime();
-    updateColour();
     updateGreeting();
   }, 1000);
 
   return (
     <>
-      <StyledGreetings colourOfHour={colourLightness}>
+      <StyledGreetings setLightness={props.setHsl}>
+        <p>I am a Web Developer with passion for the web, innovation and art.</p>
         <br />
-        <p>Good {greeting}.</p>
+        <p>Good {greeting}</p>
       </StyledGreetings>
     </>
   );
