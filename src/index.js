f0606a389079fec33ef2dd23baaed68a27959a83
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-03-28 11:14:30 +1300

    Use JS for the global style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "../src/styles.css";
+import GlobalStyle from "./Styles/global";
+import styled from "styled-components";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   rootElement
