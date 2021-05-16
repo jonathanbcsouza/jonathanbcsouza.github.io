226ea38727e531a34acfdfcd2ce04e743b761454
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-05-16 19:31:13 +1200

    Updates

---
+++ precache-manifest.660e0b55fb8147a7a17c59590cf22c6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a986ca9592ac522a79b293165bf47aaa",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "9ae41d774ab7419933c1",
+    "url": "/personal-website/static/js/2.ba188b7d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/personal-website/static/js/2.ba188b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd77860557652abda4e8",
+    "url": "/personal-website/static/js/main.644de7cc.chunk.js"
+  },
+  {
+    "revision": "551d018776f0a15d631e",
+    "url": "/personal-website/static/js/runtime-main.b5cd65b7.js"
+  }
+]);
