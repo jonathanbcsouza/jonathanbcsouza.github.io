f16f914bf6246ce3f1452402b2482e046e7b74ef
Jonathan Souza <jonathanbcsouza@gmail.com>
2023-05-30 08:28:51 +1200

    Deploy to github pages

---
+++ precache-manifest.073affd284a7cb5acfdc09220dffdff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1160754c29d3836869b4419cc942b908",
+    "url": "/personalwebsite/index.html"
+  },
+  {
+    "revision": "428a336b029aae331cc9",
+    "url": "/personalwebsite/static/js/2.c85a00b2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/personalwebsite/static/js/2.c85a00b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b91118723fb6ccb340",
+    "url": "/personalwebsite/static/js/main.df4f7a62.chunk.js"
+  },
+  {
+    "revision": "74c08130c02602688ae3",
+    "url": "/personalwebsite/static/js/runtime-main.c30763b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personalwebsite/precache-manifest.10da29cde21535302de874f37a844713.js"
+  "/personalwebsite/precache-manifest.073affd284a7cb5acfdc09220dffdff5.js"
 );
 
 self.addEventListener('message', (event) => {
