a6bf75b9a1e9ed79d823fcc6ab566970b3684745
jonathanbcsouza <jonathanbcsouza@gmail.com>
2023-10-15 19:43:47 +1300

    refactor: move iframe data into existing json file

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-import Iframe from 'react-iframe'
+import React from 'react';
+import styled from 'styled-components';
+import Iframe from 'react-iframe';
+import data from '../data/data.json';
 
 const StyledDiv = styled.div`
   display: flex;
@@ -20,30 +21,22 @@ const StyledDiv = styled.div`
   iframe {
     margin: 5% 8px 8px 8px;
   }
-`
+`;
 
 export const Music = () => {
   return (
     <StyledDiv>
-      <Iframe
-        style="border-radius:12px"
-        src="https://open.spotify.com/embed/artist/3AxDKUFfndRKbL2W0sxPZ0?utm_source=generator&theme=0"
-        width="100%"
-        height="352"
-        frameBorder="0"
-        allowfullscreen=""
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></Iframe>
-
-      <Iframe
-        width="100%"
-        height="352"
-        scrolling="no"
-        frameborder="no"
-        allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/326981607&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></Iframe>
+      {data.music_links.map((each, index) => (
+        <Iframe
+          key={index}
+          src={each.src}
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></Iframe>
+      ))}
     </StyledDiv>
-  )
-}
+  );
+};
